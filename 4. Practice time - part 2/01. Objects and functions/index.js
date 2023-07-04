5d5704c4079fd5a20d9426acde89cdb91cafd4f2
Johnny Hoang Dinh Huy Nguyen <jhdn48823@edu.ucl.dk>
2023-07-04 04:35:47 +0200

    Finished Practice time - part 2.

--- 4. Practice time - part 2/01. Objects and functions/index.js
+++ 4. Practice time - part 2/01. Objects and functions/index.js
@@ -1,6 +1,18 @@
 // Create a person object that contains three keys: name, age, and county.
 // Use yourself as an example to set the values for name, age, and country
 
+let person = {
+    name: "Johnny",
+    age: 25,
+    country: "Denmark"
+}
+
+function logData() 
+{
+    console.log(person.name + " is " + person.age + " years old and lives in " + person.country)
+}
+
+logData()
 // Create a function, logData(), that uses the person object to create a
 // string in the following format: 
 // "Per is 35 years old and lives in Norway"
--- 4. Practice time - part 2/06. Rock papers scissors/index.js
+++ 4. Practice time - part 2/06. Rock papers scissors/index.js
@@ -2,4 +2,8 @@ let hands = ["rock", "paper", "scissor"]
 
 // Create a function that returns a random item from the array
 
-
+function returnItem()
+{
+    let randomIndex = Math.floor(Math.random() * 3) 
+    return hands[randomIndex]
+}
